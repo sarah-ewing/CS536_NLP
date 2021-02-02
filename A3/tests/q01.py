778d8c5e7c161d955217e5507c78609da17a39d3
Sarah Ewing <onyxsarah@gmail.com>
2021-02-02 13:07:49 -0700

    unzipped the HW3

---
+++ A3/tests/q01.py
@@ -0,0 +1,15 @@
+test = {
+  'name': '1.0',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+       
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
